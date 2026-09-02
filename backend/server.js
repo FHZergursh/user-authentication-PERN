5aefd3118f030301b67dedbcabda72990d9a1e29
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-09-02 03:12:51 +0100

    add cors, add protect method, fix errors with username
    title

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ import express from "express"
 import setupUserDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
+import cors from "cors"
+import authRoutes from "./routes/auth.js"
+
 
 dotenv.config()
 const app = express();
+app.use(cors())
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.get("/", (req, res) => {
-  res.send("Hello wrold")
-})
+app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT
 
